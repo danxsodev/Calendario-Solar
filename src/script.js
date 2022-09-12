2d4df565ab8b3c95f1b7fa42a07b12dea5516e60
danxsodev <102252843+danxsodev@users.noreply.github.com>
2022-09-12 00:49:46 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,28 @@
+function Show() {
+
+const mes = new Array(13);
+    mes [0] = "Inválido";
+    mes [1] = "Janeiro";
+    mes [2] = "Fevereiro";
+    mes [3] = "Março";
+    mes [4] = "Abril";
+    mes [5] = "Maio";
+    mes [6] = "Junho";
+    mes [7] = "Julho";
+    mes [8] = "Agosto";
+    mes [9] = "Setembro";
+    mes [10] = "Outubro";
+    mes [11] = "Novembro";
+    mes [12] = "Dezembro";
+
+const semana = new Array(8);
+    semana[0] = "Inválido";
+    semana[1] = "Domingo";
+    semana[2] = "Segunda";
+    semana[3] = "Terça";
+    semana[4] = "Quarta";
+    semana[5] = "Quinta";
+    semana[6] = "Sexta";
+    semana[7] = "Sábado";
+
+}
